16ff2b1769275a474567ccfd03307624ae95a5b2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-08-07 16:01:00 -0400

    Added multiple projects

--- COMP123-S2019-Lesson13B-Part1/Views/MasterForm.Designer.cs
+++ COMP123-S2019-Lesson13B-Part1/Views/MasterForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace COMP123_S2019_Lesson13B_Part1
+﻿namespace COMP123_S2019_Lesson13B_Part1.Views
 {
-    partial class Form1
+    partial class MasterForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MasterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "MasterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Master Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
